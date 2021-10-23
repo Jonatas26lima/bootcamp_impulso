ab917dd603b9d075c936611dce31c0755f7dbad6
jonatas lima <dcpontofinal@gmail.com>
2021-10-23 20:22:53 -0300

    desafios básicos de JS concluidos

---
+++ Desafios_JS_basicos/tabuada.js
@@ -0,0 +1,12 @@
+let N = 140;
+
+console.log("1 x " + N + " = " + (N * 1));
+console.log("2 x " + N + " = " + (N * 2));
+console.log("3 x " + N + " = " + (N * 3));
+console.log("4 x " + N + " = " + (N * 4));
+console.log("5 x " + N   + " = " +  (N * 5));
+console.log("6 x " +  N   + " = " + (N * 6));
+console.log( "6 x " +  N   + " = " + (N * 7));                               
+console.log("6 x " +   N  + " = " + (N * 8));         
+console.log("6 x " +   N  + " = " + (N * 9));                                          
+console.log("6 x " +   N  + " = " + (N * 10));                                          
